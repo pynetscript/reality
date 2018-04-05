5f5eb41f4cadef730347fc6aa41c6a9472aa04a5
Aleks Lambreca <31127654+pynetscript@users.noreply.github.com>
2018-04-05 21:13:49 +0300

    Update cmdrunner.py

--- cmdrunner.py
+++ cmdrunner.py
@@ -3,7 +3,7 @@
 ###############################################################################
 # Written by:           Aleks Lambreca
 # Creation date:        20/03/2018
-# Last modified date:   24/03/2018
+# Last modified date:   05/04/2018
 # Version:              v1.1
 #
 # Script use:           SSH into Cisco IOS devices and run config/show commands
