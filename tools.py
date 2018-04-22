fa2a2028cae612b482223ff529568ee4384bb9bb
Aleks Lambreca <31127654+pynetscript@users.noreply.github.com>
2018-04-22 21:08:11 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,13 +1,23 @@
+###############################################################################
+# Written by:           Aleks Lambreca
+# Creation date:        24/03/2018
+# Last modified date:   22/04/2018
+###############################################################################
+
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+# Standard library modules
 from colorama import init
 from colorama import Fore
 from colorama import Style
 from getpass import getpass
+from collections import Counter
 
-# Function - Get input that is Py2/Py3 compatible.
+
+# Get input that is Py2/Py3 compatible.
 def get_input(prompt=''):
     try:
         line = raw_input(prompt)
@@ -16,9 +26,8 @@ def get_input(prompt=''):
     return line
 
 
-# Function - Prompts for, and returns a username and password.
+# Prompts for, and returns a username and password.
 def get_credentials():
-    print(Fore.WHITE + '='*79 + Style.RESET_ALL)
     username = get_input('Username: ')
     password = None
     while not password:
@@ -29,3 +38,11 @@ def get_credentials():
             password = None
             continue
         return username, password
+
+
+# Count how many chars are in a string
+def count_letters(x):
+    counter = Counter()
+    for word in x.split():
+        counter.update(word)
+    return sum(counter.values())
