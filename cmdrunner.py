4d064a0cd04c140e1b9d7373a09f75001d0a1b6f
Aleks Lambreca <31127654+pynetscript@users.noreply.github.com>
2018-04-22 21:09:33 +0300

    Update cmdrunner.py

--- cmdrunner.py
+++ cmdrunner.py
@@ -3,7 +3,7 @@
 ###############################################################################
 # Written by:           Aleks Lambreca
 # Creation date:        24/03/2018
-# Last modified date:   15/04/2018
+# Last modified date:   22/04/2018
 # Version:              v1.2
 #
 # Script use:           SSH into Cisco IOS devices and run show commands
@@ -27,7 +27,7 @@
 #                       Errors in screen
 #                       Progress bar
 #                       Statistics
-#                       Log erros in cmdrunner.log
+#                       Log success/erros in cmdrunner.log
 #                       Travis CI build notification to Slack private channel
 ###############################################################################
 
@@ -45,6 +45,7 @@ import datetime
 import time
 import logging
 import os
+import re
 
 from colorama import init
 from colorama import Fore
@@ -76,7 +77,7 @@ netmiko_ex_auth = (netmiko.ssh_exception.NetMikoAuthenticationException)
 
 # If arguments not equal to 3 we get an error.
 if len(sys.argv) != 3:
-    print('>> Usage: cmdrunner.py /x.json /x.txt')
+    print('>> Usage:', sys.argv[0].split('/')[-1], '/x.json /x.txt')        #Changed
     exit()
 
 
@@ -85,6 +86,11 @@ with open(sys.argv[1]) as dev_file:
 
 with open(sys.argv[2]) as cmd_file:
     commands = cmd_file.readlines()
+
+
+# Prompt for Change Control/Ticket
+print(Fore.WHITE + '='*79 + Style.RESET_ALL)
+cc = tools.get_input('Change Control/Ticket: ')
 
 
 # Prompt for username and password
@@ -118,12 +124,24 @@ for device in pbar(devices):
 
         current_timestamp = datetime.datetime.now()
         current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
-        print(current_time, '- Successfully connected -', device['ip'])
+        #success_connected = (current_time, '- Successfully connected -', device['ip'])
+        success_connected = (current_time, '- Connection to device successful:', device['ip'])
+        success_connected_str = ' '.join(success_connected)
+        print(success_connected_str)
         print('-'*79)
+
+        # Log the successful connection on the working directory in cmdrunner.log
+        # Parse out the datestamp
+        regex = r'(\d+/\d+/\d+\s+\d+:\d+:\d+\s+-\s+)(.*)'
+        m_conn = re.match(regex, success_connected_str)
+        logger.info(m_conn.group(2))
 
         # Get device's "hostname" from netmiko, and "ip" from .json
         hostname = connection.base_prompt
         ip = (device['ip'])
+
+        # Log "start" locally on the device.
+        connection.send_command('send log 6 "Begin Change Control/Ticket: {}"'.format(cc))
 
         # Send each command from "x.txt" to device (3rd argument).
         for command in commands:
@@ -135,22 +153,30 @@ for device in pbar(devices):
                 print(connection.send_command(command))
                 print('\n' + '-'*79)
 
+        # Log "end" locally on the device.
+        connection.send_command('send log 6 "End Change Control/Ticket: {}"'.format(cc))
+
         # Disconnect SSH session.
         connection.disconnect()
+
+        # Log the successful configuration on the working directory in cmdrunner.log
+        success_configured = ('Configuration to device successful:', device['ip'])
+        success_configured_str = ' '.join(success_configured)
+        logger.info(success_configured_str)
 
 
     except netmiko_ex_auth as ex_auth:
         current_timestamp = datetime.datetime.now()
         current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
-        print(Fore.RED + current_time, '- Authentication error -', device['ip'] + Style.RESET_ALL)
-        # Log the error on the working directory in cmdrunner.log
+        print(Fore.RED + current_time, '- Authentication error:', device['ip'] + Style.RESET_ALL)
+        # Log the authentication error on the working directory in cmdrunner.log
         logger.warning(ex_auth)
 
     except netmiko_ex_time as ex_time:
         current_timestamp = datetime.datetime.now()
         current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
-        print(Fore.RED + current_time, '- TCP/22 connectivity error -', device['ip'] + Style.RESET_ALL)
-        # Log the error on the working directory in cmdrunner.log
+        print(Fore.RED + current_time, '- TCP/22 connectivity error:', device['ip'] + Style.RESET_ALL)
+        # Log the TCP/22 connectivity error on the working directory in cmdrunner.log
         logger.warning(ex_time)
 
 
@@ -163,12 +189,19 @@ total_time = end_timestamp - start_timestamp
 total_time = str(total_time).split(".")[0]
 
 
+# Count how many chars is Change Control/Ticket and subtract 50 from it.
+# Result (rest) used in SCRIPT STATISTICS
+cc_counter = tools.count_letters(cc)
+rest = 50 - cc_counter
+
+
 # SCRIPT STATISTICS
 print(Fore.WHITE + '='*79 + Style.RESET_ALL)
 print("+" + "-"*77 + "+")
-print("|" + " "*30 + "SCRIPT STATISTICS" +       " "*30 + "|")
+print("|" + " "*30 + "SCRIPT STATISTICS" +      " "*30 + "|")
 print("|" + "-"*77 + "|")
-print("| Script started:          ", start_time, " "*30 + "|")
-print("| Script ended:            ", end_time,   " "*30 + "|")
-print("| Script duration (h:m:s): ", total_time, " "*42 + "|")
+print("| Change Control/Ticket:  ", cc,         " "*rest + "|")
+print("| Script started:         ", start_time, " "*31 + "|")
+print("| Script ended:           ", end_time,   " "*31 + "|")
+print("| Script duration (h:m:s):", total_time, " "*43 + "|")
 print("+" + "-"*77 + "+")
