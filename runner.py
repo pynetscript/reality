0b8f554133837f1f7a7e2661f6b0792057e4d0e0
Aleks Lambreca <31127654+pynetscript@users.noreply.github.com>
2018-08-19 19:56:19 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -11,21 +11,21 @@
 #                             Supports both IPv4 and IPv6 addresses and FQDNs
 #                             Both Py2 and Py3 compatible
 #                       The script needs 3 arguments to work:
-#                       - 1st argument: cmdrunner.py
+#                       - 1st argument: runner.py
 #                       - 2nd argument: x.json
 #                       - 3rd argument: x.txt
 #                       Note: A full command looks like:
-#                       ./cmdrunner.py router/7200.json router/cmd.txt
-#                       ./cmdrunner.py netbox.json cmd.txt
+#                       ./runner.py router/7200.json router/cmd.txt
+#                       ./runner.py netbox.json cmd.txt
 #
-# Script input:         Option 1) Local devices
+# Script input:         Option 1) Get devices via local .json file
 #                       - Specify devices as a .json file
 #                       - Note: See "router/7200.json" as an example
 #                       - Specify show commands as a .txt file
 #                       - Note: See "router/cmd.txt" as an example
 #                       - Username/Password
-#                       Option 2) GET devices from Netbox API
-#                       - Needs ./cmdrunner.py netbox.json cmd.txt to be used.
+#                       Option 2) GET devices via Netbox API
+#                       - Needs ./runner.py netbox.json cmd.txt to be used
 #                       - Specify Netbox server IP/FQDN
 #                       - Specify API Token
 #                       - Specify what to GET
@@ -35,7 +35,7 @@
 #                       Errors in screen
 #                       Progress bar
 #                       Statistics
-#                       Log success/erros in cmdrunner.log
+#                       Log success/erros in runner.log
 #                       Travis CI build notification to Slack private channel
 ###############################################################################
 
@@ -61,7 +61,7 @@ from progressbar import *
 import tools
 
 
-# Logs on the working directory on the file named cmdrunner.log
+# Logs on the working directory on the file named runner.log
 logger = logging.getLogger('__name__')
 hdlr = logging.FileHandler('runner.log')
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
@@ -101,11 +101,6 @@ print(Fore.WHITE + '='*79 + Style.RESET_ALL)
 username, password = tools.get_credentials()
 
 
-# Script start timestamp and formatting
-start_timestamp = datetime.datetime.now()
-start_time = start_timestamp.strftime('%d/%m/%Y %H:%M:%S')
-
-
 # Progress Bar
 widgets = ['\n',
            Percentage(), ' ', Bar(marker='#', left='[', right=']'),
@@ -113,93 +108,101 @@ widgets = ['\n',
 
 pbar = ProgressBar(widgets=widgets)
 
+def main():
+    # Script start timestamp and formatting
+    start_timestamp = datetime.datetime.now()
+    start_time = start_timestamp.strftime('%d/%m/%Y %H:%M:%S')
 
-for device in pbar(devices):
-    device['username'] = username
-    device['password'] = password
-    try:
+    for device in pbar(devices):
+        device['username'] = username
+        device['password'] = password
+        try:
+            print(Fore.WHITE + '='*79 + Style.RESET_ALL)
+            current_timestamp = datetime.datetime.now()
+            current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
+            print(current_time, '- Connecting to device:', device['ip'])
+
+            # SSH into each device from "x.json" (2nd argument).
+            connection = netmiko.ConnectHandler(**device)
+
+            current_timestamp = datetime.datetime.now()
+            current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
+            #success_connected = (current_time, '- Successfully connected -', device['ip'])
+            success_connected = (current_time, '- Connection to device successful:', device['ip'])
+            success_connected_str = ' '.join(success_connected)
+            print(success_connected_str)
+            print('-'*79)
+
+            # Log the successful connection on the working directory in runner.log
+            # Parse out the datestamp
+            regex = r'(\d+/\d+/\d+\s+\d+:\d+:\d+\s+-\s+)(.*)'
+            m_conn = re.match(regex, success_connected_str)
+            logger.info(m_conn.group(2))
+
+            # Get device's "hostname" from netmiko, and "ip" from .json
+            hostname = connection.base_prompt
+            ip = (device['ip'])
+
+            # Log "start" locally on the device.
+            sysargv = ' '.join(sys.argv)
+            connection.send_command('send log 6 "Begin script: {}"'.format(sysargv))
+            
+            # Send each command from "x.txt" to device (3rd argument).
+            for command in commands:
+                # If only whitespace in line do nothing and continue.
+                if command in ['\n', '\r\n']:
+                    pass
+                else:
+                    print('[{0}] [{1}] >> {2}'.format(hostname, ip, command) + '\n')
+                    print(connection.send_command(command))
+                    print('\n' + '-'*79)
+
+            # Log "end" locally on the device.
+            connection.send_command('send log 6 "End script: {}"'.format(sysargv))
+
+            # Disconnect SSH session.
+            connection.disconnect()
+
+            # Log the successful configuration on the working directory in runner.log
+            success_configured = ('Configuration to device successful:', device['ip'])
+            success_configured_str = ' '.join(success_configured)
+            logger.info(success_configured_str)
+
+
+        except netmiko_ex_auth as ex_auth:
+            current_timestamp = datetime.datetime.now()
+            current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
+            print(Fore.RED + current_time, '- Authentication error:', device['ip'] + Style.RESET_ALL)
+            # Log the authentication error on the working directory in runner.log
+            logger.warning(ex_auth)
+
+        except netmiko_ex_time as ex_time:
+            current_timestamp = datetime.datetime.now()
+            current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
+            print(Fore.RED + current_time, '- TCP/22 connectivity error:', device['ip'] + Style.RESET_ALL)
+            # Log the TCP/22 connectivity error on the working directory in runner.log
+            logger.warning(ex_time)
+
+
+        # Script end timestamp and formatting
+        end_timestamp = datetime.datetime.now()
+        end_time = end_timestamp.strftime('%d/%m/%Y %H:%M:%S')
+
+        # Script duration and formatting
+        total_time = end_timestamp - start_timestamp
+        total_time = str(total_time).split(".")[0]
+
+
+        # SCRIPT STATISTICS
         print(Fore.WHITE + '='*79 + Style.RESET_ALL)
-        current_timestamp = datetime.datetime.now()
-        current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
-        print(current_time, '- Connecting to device:', device['ip'])
-
-        # SSH into each device from "x.json" (2nd argument).
-        connection = netmiko.ConnectHandler(**device)
-
-        current_timestamp = datetime.datetime.now()
-        current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
-        #success_connected = (current_time, '- Successfully connected -', device['ip'])
-        success_connected = (current_time, '- Connection to device successful:', device['ip'])
-        success_connected_str = ' '.join(success_connected)
-        print(success_connected_str)
-        print('-'*79)
-
-        # Log the successful connection on the working directory in cmdrunner.log
-        # Parse out the datestamp
-        regex = r'(\d+/\d+/\d+\s+\d+:\d+:\d+\s+-\s+)(.*)'
-        m_conn = re.match(regex, success_connected_str)
-        logger.info(m_conn.group(2))
-
-        # Get device's "hostname" from netmiko, and "ip" from .json
-        hostname = connection.base_prompt
-        ip = (device['ip'])
-
-        # Log "start" locally on the device.
-        sysargv = ' '.join(sys.argv)
-        connection.send_command('send log 6 "Begin script: {}"'.format(sysargv))
-        
-        # Send each command from "x.txt" to device (3rd argument).
-        for command in commands:
-            # If only whitespace in line do nothing and continue.
-            if command in ['\n', '\r\n']:
-                pass
-            else:
-                print('[{0}] [{1}] >> {2}'.format(hostname, ip, command) + '\n')
-                print(connection.send_command(command))
-                print('\n' + '-'*79)
-
-        # Log "end" locally on the device.
-        connection.send_command('send log 6 "End script: {}"'.format(sysargv))
-
-        # Disconnect SSH session.
-        connection.disconnect()
-
-        # Log the successful configuration on the working directory in cmdrunner.log
-        success_configured = ('Configuration to device successful:', device['ip'])
-        success_configured_str = ' '.join(success_configured)
-        logger.info(success_configured_str)
+        print("+" + "-"*77 + "+")
+        print("|" + " "*30 + "SCRIPT STATISTICS" +      " "*30 + "|")
+        print("|" + "-"*77 + "|")
+        print("| Script started:         ", start_time, " "*31 + "|")
+        print("| Script ended:           ", end_time,   " "*31 + "|")
+        print("| Script duration (h:m:s):", total_time, " "*43 + "|")
+        print("+" + "-"*77 + "+")
 
 
-    except netmiko_ex_auth as ex_auth:
-        current_timestamp = datetime.datetime.now()
-        current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
-        print(Fore.RED + current_time, '- Authentication error:', device['ip'] + Style.RESET_ALL)
-        # Log the authentication error on the working directory in cmdrunner.log
-        logger.warning(ex_auth)
-
-    except netmiko_ex_time as ex_time:
-        current_timestamp = datetime.datetime.now()
-        current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
-        print(Fore.RED + current_time, '- TCP/22 connectivity error:', device['ip'] + Style.RESET_ALL)
-        # Log the TCP/22 connectivity error on the working directory in cmdrunner.log
-        logger.warning(ex_time)
-
-
-# Script end timestamp and formatting
-end_timestamp = datetime.datetime.now()
-end_time = end_timestamp.strftime('%d/%m/%Y %H:%M:%S')
-
-# Script duration and formatting
-total_time = end_timestamp - start_timestamp
-total_time = str(total_time).split(".")[0]
-
-
-# SCRIPT STATISTICS
-print(Fore.WHITE + '='*79 + Style.RESET_ALL)
-print("+" + "-"*77 + "+")
-print("|" + " "*30 + "SCRIPT STATISTICS" +      " "*30 + "|")
-print("|" + "-"*77 + "|")
-print("| Script started:         ", start_time, " "*31 + "|")
-print("| Script ended:           ", end_time,   " "*31 + "|")
-print("| Script duration (h:m:s):", total_time, " "*43 + "|")
-print("+" + "-"*77 + "+")
+if __name__ == "__main__":
+    main()
